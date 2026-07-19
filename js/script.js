0e19d5c80da8e3448ce87c8e34157009a3b6ad27
JTRkpp <mikkokawaii01@gmail.com>
2026-07-19 16:55:47 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,42 +2,42 @@ let characters = [];
 let boardCharacters = [];
 let marked = [];
 
-
 const board = document.getElementById("bingo-board");
 const message = document.getElementById("bingo-message");
 const callerResult = document.getElementById("caller-result");
 
 
+// โหลดรายชื่อตัวละคร
 fetch("data/characters.json")
-.then(response => response.json())
-.then(data => {
+    .then(response => response.json())
+    .then(data => {
 
-    characters = data;
+        characters = data;
+        createBoard();
 
-    createBoard();
-
-});
+    });
 
 
-function shuffle(array){
+// สุ่มข้อมูล
+function shuffle(array) {
 
     return array.sort(() => Math.random() - 0.5);
 
 }
 
 
-
-function createBoard(){
+// สร้างกระดาน Bingo
+function createBoard() {
 
     board.innerHTML = "";
     message.innerHTML = "";
 
-    boardCharacters = shuffle([...characters]).slice(0,25);
+    boardCharacters = shuffle([...characters]).slice(0, 25);
 
     marked = Array(25).fill(false);
 
 
-    boardCharacters.forEach((character,index)=>{
+    boardCharacters.forEach((character, index) => {
 
 
         let cell = document.createElement("div");
@@ -47,14 +47,12 @@ function createBoard(){
 
         cell.innerHTML = `
 
-        <img src="${character.image}">
-        <span>${character.name}</span>
+            <span>${character.name}</span>
 
         `;
 
 
-        cell.onclick = ()=>{
-
+        cell.onclick = () => {
 
             marked[index] = !marked[index];
 
@@ -78,34 +76,39 @@ function createBoard(){
 
 
 
-function checkBingo(){
+// ตรวจ Bingo
+function checkBingo() {
 
 
     let lines = [
 
+        // แนวนอน
         [0,1,2,3,4],
         [5,6,7,8,9],
         [10,11,12,13,14],
         [15,16,17,18,19],
         [20,21,22,23,24],
 
+        // แนวตั้ง
         [0,5,10,15,20],
         [1,6,11,16,21],
         [2,7,12,17,22],
         [3,8,13,18,23],
         [4,9,14,19,24],
 
+        // ทแยง
         [0,6,12,18,24],
         [4,8,12,16,20]
 
     ];
 
 
-    for(let line of lines){
+    for (let line of lines) {
 
-        if(line.every(i=>marked[i])){
 
-            message.innerHTML="🎉 BINGO!";
+        if (line.every(index => marked[index])) {
+
+            message.innerHTML = "🎉 BINGO!";
 
             return;
 
@@ -114,39 +117,40 @@ function checkBingo(){
     }
 
 
-    message.innerHTML="";
+    message.innerHTML = "";
 
 }
 
 
 
+// ปุ่มสร้างกระดานใหม่
 document
-.getElementById("new-board")
-.onclick=createBoard;
+    .getElementById("new-board")
+    .onclick = createBoard;
 
 
 
-let callerPool=[];
+// Caller Mode
+
+let callerPool = [];
 
 
 document
-.getElementById("caller")
-.onclick=function(){
+    .getElementById("caller")
+    .onclick = function () {
 
 
-    if(callerPool.length===0){
+        if (callerPool.length === 0) {
 
-        callerPool=shuffle([...characters]);
+            callerPool = shuffle([...characters]);
 
-    }
-
-
-    let picked=callerPool.pop();
+        }
 
 
-    callerResult.innerHTML=
-
-    picked.name;
+        let picked = callerPool.pop();
 
 
-};
+        callerResult.innerHTML = picked.name;
+
+
+    };
