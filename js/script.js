49a16f7175c62fb29f16d9b0432207722c94090a
JTRkpp <mikkokawaii01@gmail.com>
2026-07-19 16:38:01 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,152 @@
+let characters = [];
+let boardCharacters = [];
+let marked = [];
+
+
+const board = document.getElementById("bingo-board");
+const message = document.getElementById("bingo-message");
+const callerResult = document.getElementById("caller-result");
+
+
+fetch("data/characters.json")
+.then(response => response.json())
+.then(data => {
+
+    characters = data;
+
+    createBoard();
+
+});
+
+
+function shuffle(array){
+
+    return array.sort(() => Math.random() - 0.5);
+
+}
+
+
+
+function createBoard(){
+
+    board.innerHTML = "";
+    message.innerHTML = "";
+
+    boardCharacters = shuffle([...characters]).slice(0,25);
+
+    marked = Array(25).fill(false);
+
+
+    boardCharacters.forEach((character,index)=>{
+
+
+        let cell = document.createElement("div");
+
+        cell.className = "bingo-cell";
+
+
+        cell.innerHTML = `
+
+        <img src="${character.image}">
+        <span>${character.name}</span>
+
+        `;
+
+
+        cell.onclick = ()=>{
+
+
+            marked[index] = !marked[index];
+
+            cell.classList.toggle(
+                "marked",
+                marked[index]
+            );
+
+
+            checkBingo();
+
+        };
+
+
+        board.appendChild(cell);
+
+
+    });
+
+}
+
+
+
+function checkBingo(){
+
+
+    let lines = [
+
+        [0,1,2,3,4],
+        [5,6,7,8,9],
+        [10,11,12,13,14],
+        [15,16,17,18,19],
+        [20,21,22,23,24],
+
+        [0,5,10,15,20],
+        [1,6,11,16,21],
+        [2,7,12,17,22],
+        [3,8,13,18,23],
+        [4,9,14,19,24],
+
+        [0,6,12,18,24],
+        [4,8,12,16,20]
+
+    ];
+
+
+    for(let line of lines){
+
+        if(line.every(i=>marked[i])){
+
+            message.innerHTML="🎉 BINGO!";
+
+            return;
+
+        }
+
+    }
+
+
+    message.innerHTML="";
+
+}
+
+
+
+document
+.getElementById("new-board")
+.onclick=createBoard;
+
+
+
+let callerPool=[];
+
+
+document
+.getElementById("caller")
+.onclick=function(){
+
+
+    if(callerPool.length===0){
+
+        callerPool=shuffle([...characters]);
+
+    }
+
+
+    let picked=callerPool.pop();
+
+
+    callerResult.innerHTML=
+
+    picked.name;
+
+
+};
